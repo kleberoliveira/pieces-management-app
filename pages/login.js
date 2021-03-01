1a91673893ba2a6f1534b7eeef89e8300628a83e
Kleber Oliveira <kleber.oliveira@sharecare.com>
2021-03-01 00:54:19 -0300

    feat: remove remember me button

--- pages/login.js
+++ pages/login.js
@@ -42,18 +42,6 @@ export default function Login() {
                                             placeholder="Password"
                                         />
                                     </div>
-                                    <div>
-                                        <label className="inline-flex items-center cursor-pointer">
-                                            <input
-                                                id="customCheckLogin"
-                                                type="checkbox"
-                                                className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                                            />
-                                            <span className="ml-2 text-sm font-semibold text-gray-700">
-                                                Remember me
-                                            </span>
-                                        </label>
-                                    </div>
 
                                     <div className="text-center mt-6">
                                         <button
